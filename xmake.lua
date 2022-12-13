1ef427c372b8bcdf9af5c64f127e9922e9c94b17
carlcc <carlmarxchen@foxmail.com>
2022-12-13 11:46:00 +0800

    add 'jsonmapper-use-bundled-rapidjson' option to xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -5,13 +5,25 @@ option("jsonmapper-build-example")
     set_showmenu(true)
 option_end()
 
-add_requires("rapidjson")
+option("jsonmapper-use-bundled-rapidjson")
+    set_default(false)
+    set_showmenu(true)
+option_end()
+
+if not has_config("jsonmapper-use-bundled-rapidjson") then
+    add_requires("rapidjson")
+end
 
 target("jsonmapper")
     set_kind("headeronly")
     set_languages("cxx17")
     add_includedirs("include", { interface = true })
-    add_packages("rapidjson", { public = true })
+
+    if has_config("jsonmapper-use-bundled-rapidjson") then
+        add_includedirs("thirdparty/rapidjson/include", { interface = true })
+    else
+        add_packages("rapidjson", { public = true })
+    end
 target_end()
 
 if has_config("jsonmapper-build-example") then
